5cbaf8df6dc8e822d27a0bfc0f3b36fbaa233f75
Stephen Szpak <stephenszpak@Stephens-MacBook-Air.local>
2025-06-25 18:53:10 -0500

    test for ocnfigs

--- config/prod.exs
+++ config/prod.exs
@@ -1,10 +1,10 @@
 import Config
 
 config :job_hunt, JobHunt.Repo,
-  database: System.fetch_env!("DB_NAME"),
-  username: System.fetch_env!("DB_USER"),
-  password: System.fetch_env!("DB_PASS"),
-  hostname: System.fetch_env!("DB_HOST"),
+  database: System.fetch_env!("DB_NAME") || "job_hunt_prod",
+  username: System.fetch_env!("DB_USER") || "postgres",
+  password: System.fetch_env!("DB_PASS") || "postgres",
+  hostname: System.fetch_env!("DB_HOST") || "localhost",
   pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
 
 config :job_hunt, JobHuntWeb.Router,
