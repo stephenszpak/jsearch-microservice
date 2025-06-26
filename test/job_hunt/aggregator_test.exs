0ad6e25e1c857cc8eca11994713d4adf254eacb8
Stephen Szpak <srszpak@gmail.com>
2025-06-25 21:49:39 -0500

    Add basic GitHub source and remote filter

--- test/job_hunt/aggregator_test.exs
+++ test/job_hunt/aggregator_test.exs
@@ -12,4 +12,14 @@ defmodule JobHunt.AggregatorTest do
     jobs = [%{salary: 120_000, title: "Unrelated"}]
     assert [] = Aggregator.filter(jobs, %{keyword: "Elixir"})
   end
+
+  test "filters out non-remote jobs when remote_only is true" do
+    jobs = [
+      %{salary: 120_000, title: "Elixir Dev", remote: false, location: "NY"},
+      %{salary: 120_000, title: "Remote Elixir", remote: true}
+    ]
+
+    results = Aggregator.filter(jobs, %{remote_only: true})
+    assert [%{title: "Remote Elixir"}] = results
+  end
 end
